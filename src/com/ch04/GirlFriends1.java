0f8fe0d641623b73a5fbf4403c8fefa5cfadb39b
workpc999 <61165989+workpc999@users.noreply.github.com>
2020-12-26 10:00:08 +0300

    ch04

---
+++ src/com/ch04/GirlFriends1.java
@@ -0,0 +1,17 @@
+package com.ch04;
+
+public class GirlFriends1 {
+    public static void main(String[] args) {
+        String[] friends = new String[20];
+        friends[0] = "Masha";
+        friends[1] = "matilda";
+        friends[2] = "rosa";
+        friends[3] = "Eman";
+        System.out.println("this fourth girl is " + friends[3]);
+        System.out.println("this array length " + friends.length);
+        String friends1[] = {"shimma", "Eman", "Eman", "no"};
+        String friends2 [][] = new String[20][2];
+        friends2[0][0] = "Eman";
+        friends2[0][1] = "0000000000";
+    }
+}
